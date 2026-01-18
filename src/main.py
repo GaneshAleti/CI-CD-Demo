7e2304d98cdf7636ee8240d72156fd28a93c8290
Ganeshbabu Aleti <33222513+GaneshAleti@users.noreply.github.com>
2026-01-18 16:56:00 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "hello"}
+
+
+@app.get("/details")
+def details():
+    return {"name": "FastAPI Demo app", "language": "python", "hosted_on": "k8s"}
